0eb89d49c2d9e96fcd9dac70df217de7122298bc
Vladislav Semenov <arctic.ratatat@gmail.com>
2022-01-20 16:12:08 +0300

    date test added

--- src/test/java/ru/netology/web/TestOrderForm.java
+++ src/test/java/ru/netology/web/TestOrderForm.java
@@ -2,6 +2,8 @@ package ru.netology.web;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 
@@ -10,9 +12,15 @@ import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestOrderForm {
+
+    public int days;
+
     @Test
     public void shouldAccepted() {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Краснодар");
+        $("[data-test-id=date] [placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] [placeholder='Дата встречи']").setValue(DateMeeting.dataInput(days));
     }
 }
