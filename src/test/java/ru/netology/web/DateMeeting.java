d7f82f56822a2e90f913f9a01c73af3cf4bf16b8
Vladislav Semenov <arctic.ratatat@gmail.com>
2022-01-21 11:55:05 +0300

    date check added

--- src/test/java/ru/netology/web/DateMeeting.java
+++ src/test/java/ru/netology/web/DateMeeting.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class DateMeeting {
-    static String dataInput(int days) {
+    static String dateInput(int days) {
         String inputDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         return inputDate;
     }
--- src/test/java/ru/netology/web/TestOrderForm.java
+++ src/test/java/ru/netology/web/TestOrderForm.java
@@ -12,17 +12,19 @@ import static com.codeborne.selenide.Selenide.*;
 public class TestOrderForm {
 
     public int days;
+    public String dateMeeting = DateMeeting.dateInput(3);
 
     @Test
     public void shouldAccepted() {
         open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("Краснодар");
         $("[data-test-id=date] [placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] [placeholder='Дата встречи']").setValue(DateMeeting.dataInput(days));
+        $("[data-test-id=date] [placeholder='Дата встречи']").setValue(DateMeeting.dateInput(days));
         $("[data-test-id=name] [name='name']").setValue("Валерий Копин-Боков");
         $("[data-test-id=phone] [name='phone']").setValue("+79296969292");
         $("[class=checkbox__box]").click();
         $(withText("Забронировать")).click();
         $(withText("Встреча успешно забронирована на")).shouldBe(visible, Duration.ofSeconds(15));
+        $(withText(dateMeeting)).shouldBe(visible, Duration.ofSeconds(15));
     }
 }
